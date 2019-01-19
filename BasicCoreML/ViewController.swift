84259fad4f905969f9d439ea27c0f067df732a10
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-19 19:11:34 +0530

    BAsic_CoreML_Image_Checker_Swift
    BAsic_CoreML_Image_Checker_Swift

---
+++ BasicCoreML/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  BasicCoreML
+//
+//  Created by Brian Advent on 09.06.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    let model = GoogLeNetPlaces()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        let imageView = sender.view as? UIImageView
+        
+        if let imageToAnalyse = imageView?.image {
+            if let sceneLabelString = sceneLabel(forImage: imageToAnalyse) {
+                categoryLabel.text = sceneLabelString.uppercased()
+            }
+        }
+        
+        
+    }
+    
+    func sceneLabel (forImage image:UIImage) -> String? {
+        if let pixelBuffer = ImageProcessor.pixelBuffer(forImage: image.cgImage!) {
+            guard let scene = try? model.prediction(sceneImage: pixelBuffer) else {fatalError("Unexpected runtime error")}
+            return scene.sceneLabel
+            
+        }
+        
+        return nil
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
